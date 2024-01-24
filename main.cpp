4d9368936132a6da5cd503f7c3e013377bbcb1a2
michał <michal.maziarz12@gmail.com>
2024-01-25 00:00:42 +0100

    groupped results

--- main.cpp
+++ main.cpp
@@ -19,36 +19,38 @@ int main(int argc, char *argv[]) {
 
         auto matrix = PeaUtils::readMatrixFromXmlFile(fileName);
         GeneticAlgorithm::solve(matrix, seconds, population, method);
-
     }
+    auto matrix = PeaUtils::readMatrixFromXmlFile("../RESOURCES/ftv170.xml");
+    GeneticAlgorithm::solve(matrix, 120, 1000, "OX");
+//    pmxTest();
 }
 
 void oxTest() {
-    auto matrix = PeaUtils::generateRandomTSPInstance(9);
-    int arr1[] = {1, 2, 3, 4, 5, 6, 7,8, 9};
-    int arr2[] = {9, 3, 7, 8, 2, 6, 5, 1, 4};
-    for (int i = 0; i < 9; i++) {
+    auto matrix = PeaUtils::generateRandomTSPInstance(6);
+    int arr1[] = {1, 2, 3, 4, 5, 6};
+    int arr2[] = {5, 1, 3, 6, 2, 4};
+    for (int i = 0; i < 6; i++) {
         arr1[i]--;
         arr2[i]--;
     }
-    auto parent1 = new GeneticPath(9, arr1, 0);
-    auto parent2 = new GeneticPath(9, arr2, 0);
+    auto parent1 = new GeneticPath(6, arr1, 0);
+    auto parent2 = new GeneticPath(6, arr2, 0);
     auto result = GeneticAlgorithm::orderCrossover(parent1, parent2, matrix);
-    cout << PeaUtils::arrayToString(9, result->getArray());
+    cout << PeaUtils::arrayToString(6, result->getArray());
 
 }
 
 void pmxTest() {
-    auto matrix = PeaUtils::generateRandomTSPInstance(9);
-    int arr1[] = {1, 2, 3, 4, 5, 6, 7,8, 9};
-    int arr2[] = {9, 3, 7, 8, 2, 6, 5, 1, 4};
-    for (int i = 0; i < 9; i++) {
+    auto matrix = PeaUtils::generateRandomTSPInstance(6);
+    int arr1[] = {1, 2, 3, 4, 5, 6};
+    int arr2[] = {5, 1, 3, 6, 2, 4};
+    for (int i = 0; i < 6; i++) {
         arr1[i]--;
         arr2[i]--;
     }
-    auto parent1 = new GeneticPath(9, arr1, 0);
-    auto parent2 = new GeneticPath(9, arr2, 0);
+    auto parent1 = new GeneticPath(6, arr1, 0);
+    auto parent2 = new GeneticPath(6, arr2, 0);
     auto result = GeneticAlgorithm::partiallyMatchedCrossover(parent1, parent2, matrix);
-    cout << PeaUtils::arrayToString(9, result->getArray());
+    cout << PeaUtils::arrayToString(6, result->getArray());
 
 }
